22835b93362f2399187ecbde298c0e4f1f755e07
mkbeefcake <mkblockchaindev@outlook.com>
2025-09-09 10:01:37 -0400

    installed twilio

--- app.py
+++ app.py
@@ -43,6 +43,21 @@ def equal_ignore_case(a, b):
 def index():
     return render_template('index.html', rooms=room_lists, bookings=bookings)
 
+@app.route("/book_dentist", methods=['POST'])
+def book_dentist():
+    data = request.json()
+    if not data:
+        return jsonify(status="Invalid or missing parameters", data=[])
+    
+    customer_number = data.get("customer_number")
+    patient_name = data.get('patient_name')
+    appointment_type = data.get("appointment_type")
+    customer_type = data.get("customer_type")
+    appointment_datetime = data.get("appointment_datetime")
+
+
+    
+    pass
 
 @app.route('/book', methods=['POST'])
 def book():
